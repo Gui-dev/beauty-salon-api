928036eaf61577585d5939d80fae6f1f3e874852
Gui Silva <adrianon10@gmail.com>
2023-06-14 03:40:46 -0300

    feat: add setErrorHandler

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
-import Fastify from 'fastify'
+import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
 
 import { userRoutes } from './routes/users.routes'
+import { AppError } from './errors/app-error'
 
 const app = Fastify()
 const PORT = 3333 || process.env.PORT
 
 app.register(userRoutes)
+
+app.setErrorHandler(
+  (error: Error, request: FastifyRequest, reply: FastifyReply) => {
+    if (error instanceof AppError) {
+      reply.status(error.statusCode).send({ error: error.message })
+    }
+    console.log('ERROR: ', error)
+    return reply.status(500).send({ error: 'Internal Server Error' })
+  },
+)
 
 app
   .listen({
