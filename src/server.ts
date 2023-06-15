6df3403c3c4d3a90993a5ac77157b530a7256181
Gui Silva <adrianon10@gmail.com>
2023-06-15 04:35:10 -0300

    feat: add register fastify multipart and set error handler ZodError

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,31 @@
 import Fastify, { FastifyReply, FastifyRequest } from 'fastify'
+import multipart from '@fastify/multipart'
 
 import { userRoutes } from './routes/users.routes'
 import { AppError } from './errors/app-error'
+import { ZodError } from 'zod'
 
 const app = Fastify()
 const PORT = 3333 || process.env.PORT
 
+app.register(multipart)
 app.register(userRoutes)
 
-app.setErrorHandler(
-  (error: Error, request: FastifyRequest, reply: FastifyReply) => {
-    if (error instanceof AppError) {
-      reply.status(error.statusCode).send({ error: error.message })
+app.setErrorHandler((error, request: FastifyRequest, reply: FastifyReply) => {
+  if (error instanceof AppError) {
+    reply.status(error.statusCode).send({ error: error.message })
+  }
+  if (error instanceof ZodError) {
+    const toSend = {
+      message: 'Validation error',
+      errors: JSON.parse(error.message),
+      statusCode: error.statusCode || 400,
     }
-    console.log('ERROR: ', error)
-    return reply.status(500).send({ error: 'Internal Server Error' })
-  },
-)
+    return reply.code(toSend.statusCode).send(toSend)
+  }
+  console.log('ERROR: ', error)
+  return reply.status(500).send({ error: 'Internal Server Error' })
+})
 
 app
   .listen({
