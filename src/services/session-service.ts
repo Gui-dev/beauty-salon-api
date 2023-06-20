1f58dceb4beab397a7383b3c105db88d4d979e9f
Gui Silva <adrianon10@gmail.com>
2023-06-20 04:33:29 -0300

    feat: add session service

---
+++ src/services/session-service.ts
@@ -0,0 +1,56 @@
+import { server } from './../shared'
+import { ISessionDTO } from '../dtos/session-dto'
+import { IUserRepository } from '../contracts/user-repository'
+import { UserRepository } from '../repositories/user-repository'
+import { AppError } from '../errors/app-error'
+import { HashProvider } from '../providers/hash-provider'
+
+interface ISessionServiceResponse {
+  user: {
+    id: string
+    name: string
+    email: string
+    avatar_url: string | null
+  }
+  token: string
+}
+
+export class SessionService {
+  private userRepository: IUserRepository
+  constructor() {
+    this.userRepository = new UserRepository()
+  }
+
+  public async execute({
+    email,
+    password,
+  }: ISessionDTO): Promise<ISessionServiceResponse> {
+    const userExists = await this.userRepository.findUserByEmail(email)
+    if (!userExists) {
+      throw new AppError('Email or password invalid', 401)
+    }
+    if (!(await HashProvider.compareHash(password, userExists.password))) {
+      throw new AppError('Email or password invalid password', 401)
+    }
+
+    const token = server.jwt.sign(
+      {
+        email: userExists.email,
+      },
+      {
+        sub: userExists.id,
+        expiresIn: '7d',
+      },
+    )
+
+    return {
+      user: {
+        id: userExists.id,
+        name: userExists.name,
+        email: userExists.email,
+        avatar_url: userExists.avatar_url,
+      },
+      token,
+    }
+  }
+}
