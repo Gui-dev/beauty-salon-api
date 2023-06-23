0accd1350de9cb8c7ef3b9c850c7cff6f139de51
Gui Silva <adrianon10@gmail.com>
2023-06-23 04:14:05 -0300

    feat: add helper method update

--- src/controllers/schedule-controller.ts
+++ src/controllers/schedule-controller.ts
@@ -3,6 +3,11 @@ import { createScheduleValidation } from '../validation/create-schedule-validati
 import { CreateScheduleService } from '../services/create-schedule-service'
 import { listScheduleValidation } from '../validation/list-schedule-validation'
 import { ListScheduleService } from '../services/list-schedule-service'
+import {
+  updateScheduleDateValidation,
+  updateScheduleParamIdValidation,
+} from '../validation/update-schedule-validation'
+import { UpdateScheduleService } from '../services/update-schedule-service'
 
 export class ScheduleController {
   public async index(
@@ -22,6 +27,18 @@ export class ScheduleController {
     const { name, phone, date } = createScheduleValidation.parse(request.body)
     const createScheduleService = new CreateScheduleService()
     const schedule = await createScheduleService.execute({ name, phone, date })
+    return response.status(201).send({ schedule })
+  }
+
+  public async update(
+    request: FastifyRequest,
+    response: FastifyReply,
+  ): Promise<FastifyReply> {
+    const { id } = updateScheduleParamIdValidation.parse(request.params)
+    const { date } = updateScheduleDateValidation.parse(request.body)
+    const updateScheduleService = new UpdateScheduleService()
+    const schedule = await updateScheduleService.execute({ id, date })
+
     return response.status(201).send({ schedule })
   }
 }
