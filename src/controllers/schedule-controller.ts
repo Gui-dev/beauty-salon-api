c53bde320d6c9c382bfa56408329808912461eba
Gui Silva <adrianon10@gmail.com>
2023-06-23 03:34:43 -0300

    feat: add heper method index

--- src/controllers/schedule-controller.ts
+++ src/controllers/schedule-controller.ts
@@ -1,8 +1,20 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { createScheduleValidation } from '../validation/create-schedule-validation'
 import { CreateScheduleService } from '../services/create-schedule-service'
+import { listScheduleValidation } from '../validation/list-schedule-validation'
+import { ListScheduleService } from '../services/list-schedule-service'
 
 export class ScheduleController {
+  public async index(
+    request: FastifyRequest,
+    response: FastifyReply,
+  ): Promise<FastifyReply> {
+    const { date } = listScheduleValidation.parse(request.query)
+    const listScheduleService = new ListScheduleService()
+    const schedules = await listScheduleService.execute({ date })
+    return response.status(201).send({ schedules })
+  }
+
   public async store(
     request: FastifyRequest,
     response: FastifyReply,
