2a4ee396c8ac7b14ad88a57aa2368e506aa27b80
Gui Silva <adrianon10@gmail.com>
2023-06-23 04:28:41 -0300

    feat: add checking if the time is less than 9 am and less than 19 pm

--- src/services/create-schedule-service.ts
+++ src/services/create-schedule-service.ts
@@ -1,8 +1,8 @@
 import { Schedule } from '@prisma/client'
 import { ICreateScheduleDTO } from '../dtos/create-schedule-dto'
-import { ScheduleRepository } from '../repositories/schedule.repository'
+import { ScheduleRepository } from '../repositories/schedule-repository'
 import { IScheduleRepository } from '../contracts/schedule-repository'
-import { isBefore, startOfHour } from 'date-fns'
+import { getHours, isBefore, startOfHour } from 'date-fns'
 import { AppError } from '../errors/app-error'
 
 export class CreateScheduleService {
@@ -18,17 +18,22 @@ export class CreateScheduleService {
   }: ICreateScheduleDTO): Promise<Schedule> {
     const dateFormatted = new Date(date)
     const hourStart = startOfHour(dateFormatted)
+    const hour = getHours(hourStart)
 
     if (isBefore(hourStart, new Date())) {
       throw new AppError('It is not allowed to schedule old date')
     }
+
+    if (hour < 9 || hour >= 18) {
+      throw new AppError('Create the schedule is from 9 am to 6 pm')
+    }
+
     const checkIsAvailable = await this.scheduleRepository.findScheduleByDate(
       hourStart,
     )
     if (checkIsAvailable) {
       throw new AppError('Schedule date is not available')
     }
-    console.log(hourStart)
     const schedule = await this.scheduleRepository.create({
       name,
       phone,
--- src/services/update-schedule-service.ts
+++ src/services/update-schedule-service.ts
@@ -4,7 +4,7 @@ import { IScheduleRepository } from '../contracts/schedule-repository'
 import { IUpdateScheduleDTO } from '../dtos/update-schedule-dto'
 import { AppError } from '../errors/app-error'
 import { ScheduleRepository } from '../repositories/schedule-repository'
-import { startOfHour, isBefore, isEqual } from 'date-fns'
+import { startOfHour, isBefore, isEqual, getHours } from 'date-fns'
 
 export class UpdateScheduleService {
   private scheduleRepository: IScheduleRepository
@@ -15,10 +15,16 @@ export class UpdateScheduleService {
   public async execute({ id, date }: IUpdateScheduleDTO): Promise<Schedule> {
     const dateFormatted = new Date(date)
     const hourStart = startOfHour(dateFormatted)
+    const hour = getHours(hourStart)
 
     if (isBefore(hourStart, new Date())) {
       throw new AppError('It is not allowed to schedule old date')
     }
+
+    if (hour < 9 || hour >= 18) {
+      throw new AppError('Create the schedule is from 9 am to 6 pm')
+    }
+
     const scheduleExists = await this.scheduleRepository.findScheduleById(id)
 
     if (!scheduleExists) {
