2f5c6863203f19a491004a86b80a90a25fc6275c
Gui Silva <adrianon10@gmail.com>
2023-06-26 04:06:00 -0300

    feat: add refresh token controller

---
+++ src/controllers/refresh-token-controller.ts
@@ -0,0 +1,16 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { RefreshTokenService } from '../services/refresh-token-service'
+import { refreshTokenValidation } from '../validation/refresh-token-validation'
+
+export class RefreshTokenController {
+  public async create(
+    request: FastifyRequest,
+    response: FastifyReply,
+  ): Promise<FastifyReply> {
+    const { refresh_token } = refreshTokenValidation.parse(request.body)
+    const refreshTokenService = new RefreshTokenService()
+    const refreshToken = await refreshTokenService.execute({ refresh_token })
+
+    return response.status(201).send({ refresh_token: refreshToken })
+  }
+}
